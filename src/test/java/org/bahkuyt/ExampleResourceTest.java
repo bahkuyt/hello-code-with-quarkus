536133c1b821aed2bb02989057f305bb3f2af84e
bahkuyt <mohamed.ayari@esprit.tn>
2022-07-20 19:37:07 +0200

    test launch pipelines

--- src/test/java/org/bahkuyt/ExampleResourceTest.java
+++ src/test/java/org/bahkuyt/ExampleResourceTest.java
@@ -15,7 +15,7 @@ public class ExampleResourceTest {
           .when().get("/resteasy/hello")
           .then()
              .statusCode(200)
-             .body(is("hello feres: angular ja3bassssaa"));
+             .body(is("hello feres"));
     }
 
 }
